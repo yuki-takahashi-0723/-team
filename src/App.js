baf995e85242342faac720bc408d514317238838
くわけん <kuwaken@kuwakennoMacBook-Air.local>
2021-02-13 15:49:25 +0900

    更新

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Special from './Special'
+
 
 const App = () => {
+
     return (
-        <h1>test</h1>
+        <>
+            <h1>test</h1>
+            <Special />
+        </>
+
     )
 }
 
---
+++ src/Special.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+//apiのjsonデータを取得して、表示させるのが課題
+const Special = () => {
+
+    const [features, setFeatures] = useState([])
+
+    useEffect(() => {
+        fetch('http://web-tas.net/shop/feature.php')
+            .then(response => {
+                //console.log(response.json())
+                return response.json()
+            }).then(json => {
+                console.log(json)
+                const featureDetails = json.results.special
+                setFeatures(featureDetails)
+                console.log(json)
+
+            })
+    }, [])
+
+    return (
+        features.map((feature, index) => { //mapを使用する際は原則keyが必要
+            //code,名前をオブジェクトと指定している
+            const FeaturesArray = {
+                featureCode: feature.code,
+                featureName: feature.name
+            }
+            console.log(FeaturesArray)
+            return ( //tostring()はオブジェクトを表す文字列を返します
+                <div key={index.toString()}>
+                    <p>{FeaturesArray.featureCode}</p>
+                    <p>{FeaturesArray.featureName}</p>
+                </div>
+            )
+        })
+    )
+}
+
+export default Special
